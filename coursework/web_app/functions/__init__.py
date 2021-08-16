1095f9d145b27cc1146ced9b2d5f27bc8d7c0a18
Rimich666 <89179699365@mail.ru>
2021-08-16 21:11:03 +0400

    requirements.txt

--- coursework/web_app/functions/__init__.py
+++
@@ -1,5 +0,0 @@
-from .query_table import QueryTable
-from .trial_data import (
-    make_test_data,
-    delete_data
-)
--- coursework/web_app/views/__init__.py
+++
@@ -1,9 +0,0 @@
-#from .products import products_app
-from .admin import admin_app
-from .login import login_app
-from .orders import orders_app
-
-__all__ = (
-    #products_app,
-    admin_app
-)
